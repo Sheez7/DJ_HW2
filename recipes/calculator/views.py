6fd0f8410ff9800b9d5f4e6accd28163466be395
Sheez7 <sheez2904@gmail.com>
2023-09-02 11:39:04 +0300

    hw for netology mentors

---
+++ recipes/calculator/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+def recipe(request, recipe):
+    if recipe in DATA:
+        servings = request.GET.get('servings', 1)
+        data = DATA[recipe].copy()
+
+        for ingredient, amount in data.items():
+            data[ingredient] = amount * int(servings)
+
+        context = {
+            'recipe':
+               data
+        }
+    
+        return render(request, 'calculator/index.html', context)
+    else:
+        return render(request, 'calculator/index.html')
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
